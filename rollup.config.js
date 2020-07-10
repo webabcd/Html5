8f92c0f8d031011241e6707355024f0f2292810c
webabcd <webabcd@hotmail.com>
2020-07-10 17:21:43 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -5,11 +5,12 @@ import commonjs from 'rollup-plugin-commonjs';
 export default {
     input: './js/main.js',
     output: {
+        name: 'html5', // 这个名字要写，不然有的时候编译会有警告
         file: './dist/main.js',
         format: 'iife'
     },
     plugins: [
-        // 用于解决类似 “Unresolved dependencies” 之类的警告
+        // 用于解决类似 Unresolved dependencies 或者 'xxx' is imported by ***.js, but could not be resolved – treating it as an external dependency 之类的警告
         nodeResolve(),
         // 用于将 commonjs 转换为 es6
         commonjs({
